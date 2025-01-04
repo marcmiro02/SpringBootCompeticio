46ede28db535db0cb8c361bfdf83635f22ed6936
Marc Miró <relicanttravis@gmail.com>
2025-01-04 16:56:09 +0100

    PUJAR PROJECTE

---
+++ src/test/java/com/example/gestorcompeticions/GestorCompeticionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestorcompeticions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorCompeticionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
